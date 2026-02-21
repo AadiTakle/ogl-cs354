e2ac96bdd6f85a735e5fea6ee038a08345215061
AadiTakle <aadiatakle@gmail.com>
2026-02-21 13:52:20 -0600

    tutorial 1 - already mostly done for me

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -47,9 +47,6 @@ int main( void )
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
 	do{
 		glClear(GL_COLOR_BUFFER_BIT);
 
