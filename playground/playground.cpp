5ee6f994302fbfcca62d339773b3c1a944816adb
AadiTakle <aadiatakle@gmail.com>
2026-02-21 20:11:38 -0600

    cube and triangle

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -58,19 +58,24 @@ int main(void)
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+	GLuint CubeVertexArrayID;
+	glGenVertexArrays(1, &CubeVertexArrayID);
+	glBindVertexArray(CubeVertexArrayID);
+
+	GLuint TriangleVertexArrayID;
+	glGenVertexArrays(1, &TriangleVertexArrayID);
+	glBindVertexArray(TriangleVertexArrayID);
 
 	// Create and compile our GLSL program from the shaders
 	// shaders stored in exe directory, not in project for some reason...
-	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+	GLuint cubeProgramID = LoadShaders("CubeVertexShader.vertexshader", "CubeFragmentShader.fragmentshader");
+	GLuint triangleProgramID = LoadShaders("TriangleVertexShader.vertexshader", "TriangleFragmentShader.fragmentshader");
 
 
 	// Hard coded cube goes crazy
 	// Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
 	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = {
+	static const GLfloat g_cube_vertex_buffer_data[] = {
 		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
 		-1.0f,-1.0f, 1.0f,
 		-1.0f, 1.0f, 1.0f, // triangle 1 : end
@@ -109,14 +114,25 @@ int main(void)
 		1.0f,-1.0f, 1.0f
 	};
 
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	GLuint cubevertexbuffer;
+	glGenBuffers(1, &cubevertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubevertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_vertex_buffer_data), g_cube_vertex_buffer_data, GL_STATIC_DRAW);
+
+	static const GLfloat g_triangle_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f,
+	};
+
+	GLuint trianglevertexbuffer;
+	glGenBuffers(1, &trianglevertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, trianglevertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_triangle_vertex_buffer_data), g_triangle_vertex_buffer_data, GL_STATIC_DRAW);
 
 	// Vertex colors
 	// One color for each vertex. They were generated randomly.
-	static const GLfloat g_color_buffer_data[] = {
+	static const GLfloat g_cube_color_buffer_data[] = {
 		0.583f,  0.771f,  0.014f,
 		0.609f,  0.115f,  0.436f,
 		0.327f,  0.483f,  0.844f,
@@ -155,14 +171,25 @@ int main(void)
 		0.982f,  0.099f,  0.879f
 	};
 
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	GLuint cubecolorbuffer;
+	glGenBuffers(1, &cubecolorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cubecolorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_cube_color_buffer_data), g_cube_color_buffer_data, GL_STATIC_DRAW);
+
+	static const GLfloat g_triangle_color_buffer_data[] = {
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+	};
+
+	GLuint trianglecolorbuffer;
+	glGenBuffers(1, &trianglecolorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, trianglecolorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_triangle_color_buffer_data), g_triangle_color_buffer_data, GL_STATIC_DRAW);
 
 	// MVP for triangle
 	// Projection matrix: 45° Field of View, 4:3 ratio, display range: 0.1 unit <-> 100 units
-	glm::mat4 Projection = perspective(radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+	glm::mat4 Projection = perspective(radians(90.0f), (float)width / (float)height, 0.1f, 100.0f);
 
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
@@ -172,12 +199,24 @@ int main(void)
 	);
 
 	// Model matrix: an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
+	glm::mat4 CubeModel = glm::mat4(1.0f);
 	// Our ModelViewProjection: multiplication of our 3 matrices
-	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	glm::mat4 cubeMVP = Projection * View * CubeModel; // Remember, matrix multiplication is the other way around
+
+	// Model matrix: an identity matrix (model will be at the origin)
+	mat4 TriangleModel = mat4(1.0f);
+	//Translate triangle right 5 units
+	TriangleModel = translate(TriangleModel, vec3(4.0f, 0.0f, -1.0f));
+	// Rotate triangle 90 degrees around Z axis
+	// TriangleModel = rotate(TriangleModel, radians(90.0f), vec3(0.0f, 0.0f, 1.0f));
+	// Scale triangle to double size
+	//TriangleModel = scale(TriangleModel, vec3(2.0f, 2.0f, 2.0f));
+	// Our ModelViewProjection: multiplication of our 3 matrices
+	mat4 triangleMVP = Projection * View * TriangleModel; // Remember, matrix multiplication is the other way around
 
 	// Make MVP uniform in shader
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	GLuint CubeMatrixID = glGetUniformLocation(cubeProgramID, "MVP");
+	GLuint TriangleMatrixID = glGetUniformLocation(triangleProgramID, "MVP");
 
 	do {
 
@@ -185,14 +224,14 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Use our shader
-		glUseProgram(programID);
+		glUseProgram(cubeProgramID);
 
 		// Give our MVP to shader uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
+		glUniformMatrix4fv(CubeMatrixID, 1, GL_FALSE, &cubeMVP[0][0]);
 
 		// 1st attribute buffer : vertices
 		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, cubevertexbuffer);
 		glVertexAttribPointer(
 			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
 			3,                  // size
@@ -204,7 +243,7 @@ int main(void)
 
 		// 2nd attribute buffer : colors
 		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, cubecolorbuffer);
 		glVertexAttribPointer(
 			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
 			3,                                // size
@@ -214,10 +253,47 @@ int main(void)
 			(void*)0                          // array buffer offset
 		);
 
-		// Draw the triangle !
+		// Draw the cube !
 		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
 
 		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		// Use our shader
+		glUseProgram(triangleProgramID);
+
+		// Give our MVP to shader uniform
+		glUniformMatrix4fv(TriangleMatrixID, 1, GL_FALSE, &triangleMVP[0][0]);
+
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, trianglevertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0: vertex positions
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// 2nd attribute buffer : colors
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, trianglecolorbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute 1: vertex colors
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -228,9 +304,11 @@ int main(void)
 		glfwWindowShouldClose(window) == 0);
 
 	// Cleanup VBO
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
-	glDeleteProgram(programID);
+	glDeleteBuffers(1, &cubevertexbuffer);
+	glDeleteVertexArrays(1, &CubeVertexArrayID);
+	glDeleteBuffers(1, &trianglevertexbuffer);
+	glDeleteVertexArrays(1, &TriangleVertexArrayID);
+	glDeleteProgram(cubeProgramID);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
